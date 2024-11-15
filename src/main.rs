951863e2157c19da3477eb90360b51960a0632b4
Lucas <lucas.schneider96@web.de>
2024-11-15 22:14:30 +0700

    import crates and test rss and p256 field

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
+use maestro::rep3_core::share::{HasZero, RssShare};
+use maestro::share::gf8::GF8;
+
+use mpz_fields::p256::P256;
+
+
 fn main() {
-    println!("Hello, world!");
+    println!("This is Locksmith!");
+
+    // Create a new replicated secret share (using Araki optimisation)
+    let rss_share: RssShare<GF8> = RssShare::<GF8>::from(GF8::ZERO, GF8::ZERO);
+    println!("{:?}", rss_share);
+
+    // Create element of P256 field for multi-party ECDH
+    let p256_element: P256 = P256::new(0).unwrap();  
+    println!("{:?}", p256_element);
 }
